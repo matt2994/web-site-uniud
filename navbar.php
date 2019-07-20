c0a5efc943406190bea10f7b3cdbcce4a0d5f540
michele <michele.gaiarin@gmail.com>
2019-07-20 18:47:03 +0200

    Fix echo username

--- navbar.php
+++ navbar.php
@@ -19,17 +19,15 @@
         <?php
             session_start();
             if($_SESSION['username']){
-                require_once('config/mysql.php');
-                $query='SELECT * FROM user_data';
-                $result = $conn->query($query);
+                //require_once('config/mysql.php');
+                //$query='SELECT * FROM user_data';
+                //$result = $conn->query($query);
         ?>
                 <li class="nav-item">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
         <?php
                 //--Print user name --
-                while($data = $result->fetch_array()){
-                    echo $data['User'];
-                }
+                echo $_SESSION['username'];
         ?>
                 </a>
                 <div class="dropdown-menu">
@@ -39,7 +37,7 @@
                 </li>
         <?php } else { ?>
                 <li class="nav-item"> 
-                    <a id="sign-in" class="nav-link" href="#">Registrati</a>
+                    <a id="sign-in" class="nav-link" href="signin.php">Registrati</a>
                 </li>
                 <li class="nav-item"> 
                     <a id="login" class="nav-link" href="login.php">Login</a>
